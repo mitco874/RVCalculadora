16f497f08708c647fb4b785467d6d050e01425eb
mitco874 <82779170+mitco874@users.noreply.github.com>
2022-10-17 12:48:27 -0400

    added files

---
+++ src/main/java/goxr3plus/sphinx5mavenexample/application/Main.java
@@ -0,0 +1,345 @@
+package main.java.goxr3plus.sphinx5mavenexample.application;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Port;
+
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.result.WordResult;
+
+public class Main {
+	private String screen="";
+	// Necessary
+	private LiveSpeechRecognizer recognizer;
+
+	// Logger
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	/**
+	 * This String contains the Result that is coming back from SpeechRecognizer
+	 */
+	private String speechRecognitionResult;
+	
+	//-----------------Lock Variables-----------------------------
+	
+	/**
+	 * This variable is used to ignore the results of speech recognition cause actually it can't be stopped...
+	 * 
+	 * <br>
+	 * Check this link for more information: <a href=
+	 * "https://sourceforge.net/p/cmusphinx/discussion/sphinx4/thread/3875fc39/">https://sourceforge.net/p/cmusphinx/discussion/sphinx4/thread/3875fc39/</a>
+	 */
+	private boolean ignoreSpeechRecognitionResults = false;
+	
+	/**
+	 * Checks if the speech recognise is already running
+	 */
+	private boolean speechRecognizerThreadRunning = false;
+	
+	/**
+	 * Checks if the resources Thread is already running
+	 */
+	private boolean resourcesThreadRunning;
+	
+	//---
+	
+	/**
+	 * This executor service is used in order the playerState events to be executed in an order
+	 */
+	private ExecutorService eventsExecutorService = Executors.newFixedThreadPool(2);
+	
+	//------------------------------------------------------------------------------------
+	
+	/**
+	 * Constructor
+	 */
+	public Main() {
+		
+		// Loading Message
+		logger.log(Level.INFO, "Loading Speech Recognizer...\n");
+		
+		// Configuration
+		Configuration configuration = new Configuration();
+		
+		// Load model from the jar
+		configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+		configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+		//====================================================================================
+		//=====================READ THIS!!!===============================================
+		//Uncomment this line of code if you want the recognizer to recognize every word of the language 
+		//you are using , here it is English for example	
+		//====================================================================================
+		configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+
+		//====================================================================================
+		//=====================READ THIS!!!===============================================
+		//If you don't want to use a grammar file comment below 3 lines and uncomment the above line for language model	
+		//====================================================================================
+		
+		// Grammar
+		configuration.setGrammarPath("resource:/grammars");
+		configuration.setGrammarName("grammar");
+		configuration.setUseGrammar(true);
+		
+		try {
+			recognizer = new LiveSpeechRecognizer(configuration);
+		} catch (IOException ex) {
+			logger.log(Level.SEVERE, null, ex);
+		}
+		
+		// Start recognition process pruning previously cached data.
+		// recognizer.startRecognition(true);
+		
+		//Check if needed resources are available
+		startResourcesThread();
+		//Start speech recognition thread
+		startSpeechRecognition();
+	}
+	
+	//-----------------------------------------------------------------------------------------------
+	
+	/**
+	 * Starts the Speech Recognition Thread
+	 */
+	public synchronized void startSpeechRecognition() {
+		
+		//Check lock
+		if (speechRecognizerThreadRunning)
+			logger.log(Level.INFO, "Speech Recognition Thread already running...\n");
+		else
+			//Submit to ExecutorService
+			eventsExecutorService.submit(() -> {
+				
+				//locks
+				speechRecognizerThreadRunning = true;
+				ignoreSpeechRecognitionResults = false;
+				
+				//Start Recognition
+				recognizer.startRecognition(true);
+				
+				//Information			
+				logger.log(Level.INFO, "You can start to speak...\n");
+				
+				try {
+					while (speechRecognizerThreadRunning) {
+						/*
+						 * This method will return when the end of speech is reached. Note that the end pointer will determine the end of speech.
+						 */
+						SpeechResult speechResult = recognizer.getResult();
+						
+						//Check if we ignore the speech recognition results
+						if (!ignoreSpeechRecognitionResults) {
+							
+							//Check the result
+							if (speechResult == null)
+								logger.log(Level.INFO, "I can't understand what you said.\n");
+							else {
+								
+								//Get the hypothesis
+								speechRecognitionResult = speechResult.getHypothesis();
+								
+								//You said?
+								System.out.println("You said: [" + speechRecognitionResult + "]\n");
+								calc(speechRecognitionResult);
+
+							}
+						} else
+							logger.log(Level.INFO, "Ingoring Speech Recognition Results...");
+						
+					}
+				} catch (Exception ex) {
+					logger.log(Level.WARNING, null, ex);
+					speechRecognizerThreadRunning = false;
+				}
+				
+				logger.log(Level.INFO, "SpeechThread has exited...");
+				
+			});
+	}
+	
+	/**
+	 * Stops ignoring the results of SpeechRecognition
+	 */
+	public synchronized void stopIgnoreSpeechRecognitionResults() {
+		
+		//Stop ignoring speech recognition results
+		ignoreSpeechRecognitionResults = false;
+	}
+	
+	/**
+	 * Ignores the results of SpeechRecognition
+	 */
+	public synchronized void ignoreSpeechRecognitionResults() {
+		
+		//Instead of stopping the speech recognition we are ignoring it's results
+		ignoreSpeechRecognitionResults = true;
+		
+	}
+	
+	//-----------------------------------------------------------------------------------------------
+	
+	/**
+	 * Starting a Thread that checks if the resources needed to the SpeechRecognition library are available
+	 */
+	public void startResourcesThread() {
+		
+		//Check lock
+		if (resourcesThreadRunning)
+			logger.log(Level.INFO, "Resources Thread already running...\n");
+		else
+			//Submit to ExecutorService
+			eventsExecutorService.submit(() -> {
+				try {
+					
+					//Lock
+					resourcesThreadRunning = true;
+					
+					// Detect if the microphone is available
+					while (true) {
+						
+						//Is the Microphone Available
+						if (!AudioSystem.isLineSupported(Port.Info.MICROPHONE))
+							logger.log(Level.INFO, "Microphone is not available.\n");
+						
+						// Sleep some period
+						Thread.sleep(350);
+					}
+					
+				} catch (InterruptedException ex) {
+					logger.log(Level.WARNING, null, ex);
+					resourcesThreadRunning = false;
+				}
+			});
+	}
+	
+	/**
+	 * Takes a decision based on the given result
+	 * 
+	 * @param speechWords
+	 */
+	public void makeDecision(String speech , List<WordResult> speechWords) {
+		
+		System.out.println(speech);
+
+		
+	}
+	public void calc(String speech){
+		switch (speech){
+			case "clear":screen="";
+				break;
+			case "remove":screen=screen.substring(0,screen.length()-1);
+				break;
+			case "result":screen=ExcecuteOperation(screen);
+				break;
+			case "plus":screen+=" + ";
+				break;
+			case "minus":screen+=" - ";
+				break;
+			case "multiply":screen+=" * ";
+				break;
+			case "divide":screen+=" / ";
+				break;
+			case "one":screen+="1";
+				break;
+			case "two":screen+="2";
+				break;
+			case "three":screen+="3";
+				break;
+			case "four":screen+="4";
+				break;
+			case "five":screen+="5";
+				break;
+			case "six":screen="6";
+				break;
+			case "seven":screen+="7";
+				break;
+			case "eight":screen+="8";
+				break;
+			case "nine":screen+="9";
+				break;
+
+		}
+		System.out.println(screen);
+
+
+	}
+	public String ExcecuteOperation(String screen){
+		String[] screenData=getOperationParts(screen);
+		String result="";
+		int value=0;
+		for(int i=0; i<screenData.length; i++ ){
+			System.out.println(i+" : "+ screenData[i]);
+		}
+
+
+	String currentOperation="";
+		for(int i=0; i<screenData.length; i++ ){
+
+			if(validNumber(screenData[i])){
+				if(currentOperation.length()==0){
+					value=Integer.parseInt(screenData[i]);
+				}
+				else{
+					switch (currentOperation){
+							case "+": value=value+Integer.parseInt(screenData[i]);
+								break;
+							case "-": value=value-Integer.parseInt(screenData[i]);
+								break;
+							case "*": value=value*Integer.parseInt(screenData[i]);
+								break;
+							case "%": value=value/Integer.parseInt(screenData[i]);
+								break;
+						}
+				}
+			}
+			else{
+				currentOperation=screenData[i];
+			}
+		}
+
+		result=String.valueOf(value);
+		return result;
+	}
+
+	private boolean validNumber(String screenDatum) {
+		return (screenDatum != null && screenDatum.matches("[0-9]+"));
+	}
+
+
+	public String[] getOperationParts(String string){
+		String[] result=new String[0];
+		if (screen != null) {
+			result = string.split(" ");
+		}
+	return result;
+	}
+
+
+
+
+	public boolean getIgnoreSpeechRecognitionResults() {
+		return ignoreSpeechRecognitionResults;
+	}
+	
+	public boolean getSpeechRecognizerThreadRunning() {
+		return speechRecognizerThreadRunning;
+	}
+	
+	/**
+	 * Main Method
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Main();
+	}
+}
